eebba7f2a8a75244db4a431c8169eb7843adc9d3
Desislava Velichkova <76597321+desislavavelichkova@users.noreply.github.com>
2022-05-06 22:43:18 +0300

    Add files via upload

---
+++ Summator/Summator/Summator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Summator
+    {
+        public static int Sum(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        public static double Average(long[] arr)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum/arr.Length;
+        }
+        static void Main()
+        {
+            Console.WriteLine(Sum(new int[] { 10, 20, 30}));
+        }
+    }
+}
